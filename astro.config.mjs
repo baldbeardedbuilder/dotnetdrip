d9e765cba280a5315f613d43ac411ef968ec13ee
Michael Jolley <mike@baldbeardedbuilder.com>
2026-01-10 16:10:46 -0600

    Adding sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sentry from '@sentry/astro';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sentry({
+      project: "javascript-astro",
+      org: "bald-bearded-builder",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),]
 });
