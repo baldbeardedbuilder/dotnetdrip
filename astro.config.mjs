8cceceb402af4b567a92e4cfbf3bb540c9f6a6a1
Michael Jolley <mike@baldbeardedbuilder.com>
2026-01-16 10:10:40 -0600

    Removing Sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-import sentry from '@sentry/astro';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
 
   vite: {
     plugins: [tailwindcss()]
-  },
-
-  integrations: [sentry({
-      project: "javascript-astro",
-      org: "bald-bearded-builder",
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-    }),]
+  }
 });
--- public/sentry.server.config.js
+++
@@ -1,8 +0,0 @@
-import * as Sentry from "@sentry/astro";
-
-Sentry.init({
-  dsn: "https://05878b39fc61df5f4020a065bcf84d79@o4510688362954752.ingest.us.sentry.io/4510688369049600",
-  // Adds request headers and IP for users, for more info visit:
-  // https://docs.sentry.io/platforms/javascript/guides/astro/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
-});
