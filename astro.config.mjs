db4f14a391ef13ef52d8497ce86e53c4b0b3227b
Michael Jolley <mike@baldbeardedbuilder.com>
2026-01-27 16:36:46 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+const isProduction = process.env.CONTEXT == 'production';
+const siteUrl = isProduction ? process.env.URL : process.env.DEPLOY_URL;
+
+
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.DEPLOY_PRIME_URL || process.env.URL || 'https://dotnetdrip.com',
+  site: siteUrl || 'https://dotnetdrip.com',
   output: 'static',
 
   vite: {
